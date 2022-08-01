bb721676865ba7f282b7c16d97edcff82f2575c9
promisingdev72 <promising.dev72@gmail.com>
2022-08-01 22:25:32 +0300

    update

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,9 +1,17 @@
+import { Box } from "@mui/material";
 import * as React from "react";
+
+import "../css.css";
 
 export default function Banner() {
   return (
     <>
-      <div>This is my Banner page</div>
+      <Box className="bannerBack">
+        <Box component="img" className="logo" src="/logo.png" />
+        <Box component="img" className="unhome" src="/unhome.png" />
+        <Box component="div" className="pillar1" />
+        <Box component="div" className="pillar2" />
+      </Box>
     </>
   );
 }
--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+// import Header from "./Header";
+// import Footer from "./Footer";
 
 // ----------------------------------------------------------------------
 
 export default function MainLayout() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
