83ef05280b24210f36e78d0828577e1aeca6f69b
promisingdev72 <promising.dev72@gmail.com>
2022-08-02 05:28:16 +0300

    update

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -8,9 +8,17 @@ export default function Banner() {
     <>
       <Box className="bannerBack">
         <Box component="img" className="logo" src="/logo.png" />
-        <Box component="img" className="unhome" src="/unhome.png" />
+        <Box component="img" className="unhome" src="/unhome1.png" />
         <Box component="div" className="pillar1" />
         <Box component="div" className="pillar2" />
+        <Box
+          component="img"
+          className="horizontal_lines"
+          src="/horizontal_lines.png"
+        />
+        <Box component="p" className="banner_text1">
+          Video games
+        </Box>
       </Box>
     </>
   );
