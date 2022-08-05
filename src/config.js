eb361d6ab0eaaa8b6c9ebff81a22008bd4457afe
promisingdev72 <promising.dev72@gmail.com>
2022-08-05 17:58:33 +0300

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-export const HOST_API = "https://api-litheia.herokuapp.com";
-// export const HOST_API = "http://localhost:8080";
-
+// export const HOST_API = "https://api-litheia.herokuapp.com";
+export const HOST_API = "http://localhost:8080";
